5a875b53b3c3a8c71b46b3fb5ce762904c53d772
dean-blick <101355130+dean-blick@users.noreply.github.com>
2024-06-22 19:38:20 -0500

    initial commit

---
+++ my-website/src/components/Header.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let y;
+
+    let tabs = [
+        { name: 'About me', link: '#About'},
+        { name: 'Projects', link: '#Projects'},
+        { name: 'Experience', link: '#Experience'},
+        { name: 'Transcript', link: '#Transcript'}
+    ];
+
+</script>
+
+<header class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid ' + (y > 0 ? 'py-4 bg-slate-950 border-blue-950' : 'py-6 bg-transparent border-transparent')}>
+    <h1>
+        <b class="font-bold poppins">Dean</b> Blickenstaff
+    </h1>
+    <div class="sm:flex items-center gap-4 hidden">
+        {#each tabs as tab, index}
+            <a href={tab.link} class="duration-200 hover:text-blue-500">
+                <p>{tab.name}</p>
+            </a>
+        {/each}
+    </div>
+    <a href="#" target="_blank" class="customShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950 transition hover:bg-blue-500">
+        <h4 class="relative z-9">Contact me</h4>
+    </a>
+</header>
---
+++ my-website/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        'backgroundBlue': '#000113'
+      }
+    }
+  },
+  plugins: []
+};
