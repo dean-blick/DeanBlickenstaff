277b11153ca8baed4c421c006a87b67648b92067
dean-blick <101355130+dean-blick@users.noreply.github.com>
2024-12-17 17:22:28 -0600

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				configPath: 'wrangler.toml',
+				environment: undefined,
+				experimentalJsonConfig: false,
+				persist: false
+			}
+		}),
     alias: { 'lib/': './src/routes/lib/' },
   },
   preprocess: vitePreprocess()
