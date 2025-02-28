c0c48086ddc38507af0468c6eafce08bd7a4f763
dean-blick <steveworkless@gmail.com>
2025-02-28 14:40:27 -0600

    resources

--- src/routes/lobby/game/[id]/+server.js
+++ src/routes/lobby/game/[id]/+server.js
@@ -6,3 +6,8 @@ export async function POST({request, cookies}) {
 
     //future: if a player leaves a game during the game close the game and lobby
 }
+
+
+// great resource on http/2 and streams:
+// https://stackoverflow.com/questions/46753263/what-is-the-websockets-alternative-in-the-http-2-world
+// https://datatracker.ietf.org/doc/html/draft-mcmanus-httpbis-h2-websockets-00.html
