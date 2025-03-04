cc2661db35db06f799efa7d5513feacb939f45cb
dean-blick <101355130+dean-blick@users.noreply.github.com>
2025-03-03 23:04:37 -0600

    will revert tomorrow

--- src/routes/lobby/streamAPI/[id]/+server.ts
+++ src/routes/lobby/streamAPI/[id]/+server.ts
@@ -1,13 +1,12 @@
 import { testData } from "$lib/server/testData";
 import { ObjectId } from 'mongodb';
-import type { Stream } from "stream";
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 let streamsMap = new Map();
-let statesMap = new Map();
+let statesDict = {}
 
 
 interface StreamObject {
@@ -28,7 +27,8 @@ async function CreateReadableStream(Subscription): Promise<ReadableStream> {
     let stream = new ReadableStream({
         start: async (controller) => {
             interval = setInterval(() => {
-                controller.enqueue(Subscription) //currently, the stream cancels EVERY TIME i try to enqueue something.
+                controller.enqueue(Subscription)
+                 //currently, the stream cancels EVERY TIME i try to enqueue something.
             }, 250);
         },
         cancel() {
@@ -48,40 +48,73 @@ async function grabLobby(lobbyID): Promise<Object> {
 }
 
 
-export async function GET({params, cookies}): Promise<Response> {
+// export async function GET({params, cookies}): Promise<Response> {
     
-    console.log("getting lobby")
-    let playerID = cookies.get('playerID');
-    let lobbyID = params.id;
+//     console.log("getting lobby")
+//     let playerID = cookies.get('playerID');
+//     let lobbyID = params.id;
 
-    //grab the existing record for the lobby
-    let data = await grabLobby(params.id)
-    console.log(data[0])
+//     //grab the existing record for the lobby
+//     let data = await grabLobby(params.id)
+//     console.log(data[0])
     
-    //If there is a state record for the lobby, update it, if not create one
-    if(!statesMap.has(lobbyID)) {
-        console.log("initializing lobby state")
-        let state: LobbyStateObject = {
-            players: data[0].players,
-            IsGameRunning: false,
-            GameState: {}
-        }
-        statesMap.set(lobbyID, state)
-    } else {
-        console.log("adding new player to lobby state")
-        let state: LobbyStateObject = statesMap.get(lobbyID);
-        state.players = data[0].players;
-        statesMap.set(lobbyID, state)
-    }
+//     //If there is a state record for the lobby, update it, if not create one
+//     if(!statesMap.has(lobbyID)) {
+//         console.log("initializing lobby state")
+//         let state: LobbyStateObject = {
+//             players: data[0].players,
+//             IsGameRunning: false,
+//             GameState: {}
+//         }
+//         statesMap.set(lobbyID, state)
+//     } else {
+//         console.log("adding new player to lobby state")
+//         let state: LobbyStateObject = statesMap.get(lobbyID);
+//         state.players = data[0].players;
+//         statesMap.set(lobbyID, state)
+//     }
     
-    let stream = await CreateReadableStream(statesMap.get(lobbyID))
-    console.log(stream)
-    console.log("sending GET response")
+//     let stream = await CreateReadableStream(statesMap.get(lobbyID))
+//     console.log(stream)
+//     console.log("sending GET response")
     
+//     return new Response(stream, {
+//         headers: {
+//             'content-type': 'text/event-stream',
+//         }
+//     });
+// }
+
+export function GET({ params, cookies }) {
+    let lobbyID = params.id
+    const ac = new AbortController();
+    let interval;
+    const stream = new ReadableStream({
+      start(controller) {
+        interval = setInterval(() => {
+            if(!statesDict[params.id] != undefined){
+                let state: LobbyStateObject = {
+                    players: [],
+                    IsGameRunning: false,
+                    GameState: {}
+                }
+                statesDict[params.id] = state
+            }
+            controller.enqueue(JSON.stringify(statesDict[params.id]))
+            
+        }, 250);
+      },
+      cancel() {
+        console.log("cancel and abort");
+        ac.abort();
+      },
+    })
+
+    //make call to update the statesMap from here. --------->>> TO DO
+  
     return new Response(stream, {
-        headers: {
-            'content-type': 'text/event-stream',
-        }
+      headers: {
+        'content-type': 'text/event-stream',
+      }
     });
-}
-
+  }
